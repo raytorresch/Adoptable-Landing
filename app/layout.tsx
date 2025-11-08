1302d21c005062cdf9c0caf2217c0e243c258b1c
Ray Torres <raytorres@192.168.1.45>
2025-11-08 00:40:37 -0600

    fix: AnalyticsProvider and useSearchParams problem solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from '@/components/layout/header'
 import { Footer } from '@/components/layout/footer'
 import { AnalyticsProvider } from '@/components/providers/analytics-provider'
 import { DebugAnalytics } from '@/components/analytics/debug-analytics'
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,15 +25,17 @@ export default function RootLayout({
         {/* Google Analytics Script se carga desde el cliente */}
       </head>
       <body className={`${inter.className} antialiased`}>
-        <AnalyticsProvider>
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <main className="flex-1">
-              {children}
-            </main>
-            <Footer />
-          </div>
-        </AnalyticsProvider>
+        <Suspense fallback={null}>
+            <AnalyticsProvider>
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                <main className="flex-1">
+                  {children}
+                </main>
+                <Footer />
+              </div>
+            </AnalyticsProvider>
+        </Suspense>
 
         {/* Debug solo en desarrollo */}
         {process.env.NODE_ENV === 'development' && <DebugAnalytics />}
